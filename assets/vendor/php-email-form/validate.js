bac39e0554f49ffdddef6fd2c31ad9b0b6c7b119
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-02-14 18:24:15 +0530

    personal portfoli

---
+++ assets/vendor/php-email-form/validate.js
@@ -0,0 +1,25 @@
+// function validate(){
+//   console.log("e")
+//   var name1=document.getElementById('name1').value;
+//   var email=document.getElementById("email").value;
+//   var subject=document.getElementById("subject").value;
+//   var message=document.getElementById("message").value;
+//   var error=document.getElementById("error-message");
+// console.log(typeof name1);
+//   if(name1==null){
+//     console.log("jithij")
+//     error.innerHTML="The box is empty"
+//     return false;
+//   }
+  
+//   return true;
+// }
+// var error=document.getElementById("error-message");
+// function validateName(){
+//   var name=document.getElementById('name').value;
+//   if(name==""){
+//     error.innerHTML="name required";
+//     return false;
+//   }
+//   return true;
+// }
